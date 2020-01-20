2863cc1b6497461e69e15a56dbe4eea3f8856b76
Paxxs <me@vip.morfans.cn>
2020-01-21 00:27:48 +0800

    add Get-Myip

--- powershell/Microsoft.PowerShell_functions.ps1
+++ powershell/Microsoft.PowerShell_functions.ps1
@@ -3,7 +3,7 @@ function IsAdmin {
     return $currentPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 function Edit-Hosts {
-    Invoke-Expression "$(if (-not $(IsAdmin)) {'sudo '})$(if ($env:EDITOR -ne $null) {
+    Invoke-Expression "$(if (-not $(IsAdmin)) {'sudo '})$(if ($null -ne $env:EDITOR) {
         $env:EDITOR
     } else {
         'notepad'
@@ -11,16 +11,22 @@ function Edit-Hosts {
 }
 
 function Edit-Profile {
-    Invoke-Expression "$(if ($env:EDITOR -ne $null) {
+    Invoke-Expression "$(if ($null -ne $env:EDITOR) {
         $env:EDITOR
     } else {
         'notepad'
     }) $profile"
 }
 function Update-System {
-    #require administrator
+    # require administrator
+    # https://www.powershellgallery.com/packages/PSWindowsUpdate/2.1.1.2
     Invoke-Expression "$(if (-not $(IsAdmin)) {'sudo '})Install-WindowsUpdate -IgnoreUserInput -IgnoreReboot -AcceptAll"
     Invoke-Expression "scoop update"
     Update-Module
     Update-Help
+}
+# https://www.prajwaldesai.com/get-public-ip-address-using-powershell/
+# archive: 
+function Get-Myip {
+    Invoke-RestMethod -Uri ('https://ipinfo.io/')
 }
