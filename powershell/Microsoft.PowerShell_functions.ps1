c55ff123db399068e65b112b7cee3cfb343ca9ba
Paxxs <me@vip.morfans.cn>
2020-01-20 21:16:48 +0800

    add edit-hosts function

---
+++ powershell/Microsoft.PowerShell_functions.ps1
@@ -0,0 +1,8 @@
+function Edit-Hosts {
+    Invoke-Expression "sudo $(if ($env:EDITOR -ne $null) {
+        $env:EDITOR
+    } else {
+        'notepad'
+    }) $env:windir\system32\drivers\etc\hosts"
+}
+
